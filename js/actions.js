3638fcb0bff8b7d259fee87df85249f03de3e5c1
Suzei <odetofack@gmail.com>
2021-02-13 10:43:07 -0300

    Response on screen width change + adding more visual elements

--- js/actions.js
+++ js/actions.js
@@ -1,12 +1,23 @@
 $('.nav-button').click(function() {
-    $(".menu").fadeIn();
+    $(".menu").slideDown(200);
 });
 
 $('.close-btn').click(function() {
-    $('.menu').fadeOut();
+    $('.menu').slideUp(200);
 });
 
 $('.info-upside').click(function() {
-        $(".info-text").slideToggle(150);
+    $(".info-text").slideToggle(150);
 });
 
+$('.kiryu-info').click(function() {
+    $('.kiryu-img, .kiryu').slideToggle(100);
+});
+
+$('.majima-info').click(function() {
+    $('.majima-img, .majima').slideToggle(100);
+})
+
+$('.saejima-info').click(function() {
+    $('.saejima-img, .saejima').slideToggle(100)
+})
