d835e2d5fe2e015f681d9fecb1276fa23cb1ce3b
Suzei <odetofack@gmail.com>
2021-02-15 11:50:04 -0300

    Removing JQuery animations + adding footer

--- js/actions.js
+++ js/actions.js
@@ -5,19 +5,3 @@ $('.nav-button').click(function() {
 $('.close-btn').click(function() {
     $('.menu').slideUp(200);
 });
-
-$('.info-upside').click(function() {
-    $(".info-text").slideToggle(150);
-});
-
-$('.kiryu-info').click(function() {
-    $('.kiryu-img, .kiryu').slideToggle(100);
-});
-
-$('.majima-info').click(function() {
-    $('.majima-img, .majima').slideToggle(100);
-})
-
-$('.saejima-info').click(function() {
-    $('.saejima-img, .saejima').slideToggle(100)
-})
