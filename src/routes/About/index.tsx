b1ce6d57968dd4da31a620507205dbed427d5ec3
Vinicius <odetofack@gmail.com>
2022-09-12 22:13:16 -0300

    Adicionando libs e começando a home

---
+++ src/routes/About/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <>
+      <h2>Yakuza, lição e moral.</h2>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam tempora
+        accusamus vel dicta necessitatibus ut harum vero, nostrum accusantium
+        nobis laudantium repellat neque, animi sint tempore, eveniet inventore
+        culpa optio!
+      </p>
+    </>
+  );
+};
+
+export default About;
